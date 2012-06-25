fc66bd7049c32b1ae81e33756aee9d859b703e35
EnvisionRed <Red@Covalentcraft.com>
2012-06-25 17:35:31 -0400

    updated files to match v0.3

--- src/com/envisionred/SmartExp/SmartExp.java
+++ src/com/envisionred/SmartExp/SmartExp.java
@@ -6,28 +6,40 @@ import MetricsDependencies.Metrics;
 import com.envisionred.SmartExp.SmartExpEvents;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.text.DecimalFormat;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.bukkit.plugin.PluginDescriptionFile;
+
+import org.w3c.dom.CharacterData;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  *
  * @author EnvisionRed
  */
 public class SmartExp extends JavaPlugin{
-Logger log = Logger.getLogger("Minecraft");
+Logger log = this.getLogger();
+    PluginDescriptionFile pdfile = this.getDescription();
 @Override
 public void onDisable() {
-    log.info("[SmartExp] EnvisionRed's SmartExp disabled :(");
+    log.info("EnvisionRed's SmartExp disabled :(");
     
 }
 @Override
 public void onEnable() {
     final FileConfiguration config = this.getConfig();
-    log.info("[SmartExp] EnvisionRed's SmartExp Enabled :D");
+
+    log.info("EnvisionRed's SmartExp Enabled :D");
     Enableconfig();
     StartMetrics();
     getServer().getPluginManager().registerEvents(new SmartExpEvents(this), this);
@@ -37,7 +49,7 @@ public void onEnable() {
 public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     if (cmd.getName().equalsIgnoreCase("exp")) {
         if (args.length < 1) {
-            sender.sendMessage(ChatColor.GREEN + "SmartExp v0.3 by" + ChatColor.DARK_RED + " EnvisionRed");
+            sender.sendMessage(ChatColor.GREEN + "SmartExp version " + pdfile.getVersion() + " by" + ChatColor.DARK_RED + " EnvisionRed");
             sender.sendMessage(ChatColor.GREEN + "Do " + ChatColor.AQUA + "/exp help " + ChatColor.GREEN + "to see help for the plugin.");
             return true;
         }
@@ -130,17 +142,17 @@ if(!configFile.exists())
 public void StartMetrics() {
     boolean noMetrics = this.getConfig().getBoolean("opt-out-metrics", false);
         if (this == null) {
-            log.warning("[SmartExp]Plugin is null so metrics failed to start");
+            log.warning("Plugin is null so metrics failed to start");
         }
    else if (noMetrics == true) {
-        log.info("[SmartExp] Metrics will not start because you have opted out.");
+        log.info("Metrics will not start because you have opted out.");
     } else {
         try {
                Metrics metrics = new Metrics(this);
            metrics.start();
-           log.info("[SmartExp] Metrics started successfully for SmartExp");
+           log.info("Metrics started successfully for SmartExp");
         }catch (IOException x) {
-            log.warning("[SmartExp] Metrics failed to start for SmartExp");
+            log.warning("Metrics failed to start for SmartExp");
         }
     }
 }
@@ -156,6 +168,7 @@ public void ShowHelp(Player player) {
                 + "any of the commands for SmartExp, so no use showing the help page to you.");
     }
 }
+
 
 
 
